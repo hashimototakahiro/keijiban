0673f2937b90b3708225a7275fc101136b723598
hashimototakahiro <hashimototakahiro@hashimotonoMacBook-puro.local>
2018-10-23 14:27:30 +0900

    app/controllers/posts_controller.rb

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,5 @@
+# Applicationcontrollerクラスを継承することで、クラスがコントローラと認識される
+class PostsController < ApplicationController
+    def index
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_040118) do
+ActiveRecord::Schema.define(version: 2018_10_18_084226) do
+
+  create_table "members", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
